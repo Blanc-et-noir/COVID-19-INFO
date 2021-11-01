48987b80482b392f41c115f0102309da22a80439
Blanc-et-noir <83106564+Blanc-et-noir@users.noreply.github.com>
2021-11-01 11:39:20 +0900

    Update screeningCenter.php

--- covid19/php/screeningCenter.php
+++ covid19/php/screeningCenter.php
@@ -1,6 +1,6 @@
 <?php
     $session = curl_init();
-    $URL = "http://jrw9215.dothome.co.kr/screeningCenter.json";
+    $URL = "http://jrw9215.dothome.co.kr/covid19/json/screeningCenter.json";
     curl_setopt($session,CURLOPT_URL,$URL);
     curl_setopt($session,CURLOPT_SSL_VERIFYPEER,false);
     curl_setopt($session,CURLOPT_RETURNTRANSFER,true);
